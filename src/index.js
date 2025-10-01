19d6bda7c01c3b1eeeee6931b7a3d5b0d0b62797
Pedro Hnerique Pereira dos Santos <ph0305.pereiradossantos@gmail.com>
2025-10-01 13:39:43 -0300

    Adição do Backend

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import cors from "cors"
+
+import {persons} from "./persons.js"
+
+const app = express()
+
+const port = 3333
+
+app.use(cors())
+
+app.get("/", (request, response)=> {
+    response.json(persons)
+})
+
+app.listen(port, ()=> {
+    console.log(`Server Running on port ${port}`)
+})
+
+const person = {
+    name: "Guilherme Moraes",
+    age: 16,
+    cpf: 12345678900
+
+}
---
+++ src/persons.js
@@ -0,0 +1,24 @@
+const persons = [
+    
+    {
+    name: "Guilherme Moraes",
+    age: "16 anos",
+    cpf: 12345678900
+
+    },
+
+    {
+        name: "Pedro Henrique",
+        age: "17 anos",
+        cpf: 987654321
+    },
+
+    {
+        name: "Melyssa Pereira",
+        age: "16 anos",
+        cpf: 45612378920
+    }
+    
+]
+
+export {persons}
