14fa54807128d22a773250e10bb12df63b7b28f4
Pedro Hnerique Pereira dos Santos <ph0305.pereiradossantos@gmail.com>
2025-11-05 20:26:42 -0300

    Commit da última atividade com backend e banco de dados

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from "express"
 import cors from "cors"
+import mysql2 from "mysql2"
 
 import {persons} from "./persons.js"
+
+const {DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
 
 const app = express()
 
@@ -18,12 +21,35 @@ app.post("/cadastrar", (request, response) =>{
     const { user } = request.body
     console.log(user)
 
-    response.status(201).json({ message: "Usuário cadastrado com sucesso!"})
+
+    const insertCommand = `
+        INSERT INTO pedrohenrique_02mb(name, email, password)
+        VALUES (?, ?, ?)
+    `
+
+    database.query(insertCommand, [user.name, user.email, user.password], (error) =>{
+        if(error){
+            console.log(error)
+            return
+        }
+
+        response.status(201).json({ message: "Usuário cadastrado com sucesso!" })
+    })
+
+    
 })
 
 
 
 app.listen(port, () => {
     console.log(`Server Running on port ${port}`)
+})
+
+const database = mysql2.createPool({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    connectionLimit: 10
 })
 
